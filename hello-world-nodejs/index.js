0aade362a9d8b39d99b7e1ecf5702462e0114316
Jackson Coelho <fcjack@gmail.com>
2019-12-17 07:38:33 +0100

    Hello world projects makefile build

--- hello-world-nodejs/index.js
+++ hello-world-nodejs/index.js
@@ -1,7 +1,7 @@
 exports.handler = async (event) => { 
     const response = {
         statusCode: 200,
-        body: JSON.stringify(`Welcome ${event.first_name} ${event.last_name} to the world of AWS Lambda!`),
+        body: JSON.stringify(`Welcome ${event.firstName} ${event.lastName} to the world of AWS Lambda!`),
     };
     return response;
 };
